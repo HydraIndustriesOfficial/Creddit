3e39d5e731fa38f942db3b57e3626c2cf905f5a8
HydraTech <kahlob@hydratechnologies.us>
2023-05-26 12:11:08 -0500

    changes to post.js

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import "./Post.css";
 import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
+import { BsChatLeft } from "react-icons/bs";
 
 const Post = () => {
+  const [number, setNumber] = useState(0);
   const [upVoteClicked, setUpVoteClicked] = useState(false);
   const [downVoteClicked, setDownVoteClicked] = useState(false);
   const handleUpVoteClicked = () => {
+    if (upVoteClicked) {
+      return;
+    }
     setUpVoteClicked(!upVoteClicked);
-    if (downVoteClicked === true) {
+    setNumber(number + 1);
+    if (downVoteClicked) {
       setDownVoteClicked(!downVoteClicked);
     }
   };
 
   const handleDownVoteClicked = () => {
+    if (downVoteClicked) {
+      return;
+    }
+    setNumber(number - 1);
     setDownVoteClicked(!downVoteClicked);
     if (upVoteClicked === true) {
       setUpVoteClicked(!upVoteClicked);
@@ -27,12 +37,29 @@ const Post = () => {
             color={upVoteClicked ? "green" : "white"}
             onClick={handleUpVoteClicked}
           />
-          <div className="upVotes">0</div>
+          <div className="upVotes">
+            <span>{number}</span>
+          </div>
           <AiOutlineArrowDown
             className="downVote"
             color={downVoteClicked ? "red" : "white"}
             onClick={handleDownVoteClicked}
           />
+        </div>
+        <div className="post_content_container">
+          <div className="post_information">
+            <h3>Post Information</h3>
+          </div>
+          <hr />
+          <div className="user_information">
+            <h6>UserName</h6>
+            <h6>20 hrs ago</h6>
+            <h6>
+              <BsChatLeft />
+              &nbsp;
+              8
+            </h6>
+          </div>
         </div>
       </div>
     </div>
