b7d128c66bdc0910a85a9023f35b910515689c58
HydraTech <kahlob@hydratechnologies.us>
2023-06-01 14:37:19 -0500

    main

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Honeybadger, HoneybadgerErrorBoundary } from "@honeybadger-io/react";
+
+const config = {
+  apiKey: ${{ secrets.HONEBADGER_API }},
+  environment: "production",
+};
+const honeybadger = Honeybadger.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HoneybadgerErrorBoundary honeybadger={honeybadger}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HoneybadgerErrorBoundary>
 );
 
 // If you want to start measuring performance in your app, pass a function
